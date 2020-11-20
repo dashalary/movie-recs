666c3a9306b6a59e38a158eafe1ed85f3574ff6d
Dasha Lary <dasha4988@gmail.com>
2020-11-20 16:51:36 -0500

    made user model omniauthable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_205254) do
+ActiveRecord::Schema.define(version: 2020_11_20_214748) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_11_20_205254) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
