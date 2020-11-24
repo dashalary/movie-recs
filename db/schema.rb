a9d66f86263887e2750c8044f236334147fcf93a
Dasha Lary <dasha4988@gmail.com>
2020-11-23 19:23:45 -0500

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_214748) do
+ActiveRecord::Schema.define(version: 2020_11_24_002054) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_214748) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
