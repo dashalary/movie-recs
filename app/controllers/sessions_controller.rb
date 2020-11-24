a06f7ca9d429ede74f7c235ef53818e3abd04074
Dasha Lary <dasha4988@gmail.com>
2020-11-23 19:09:47 -0500

    added links to nav

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user), notice: "Welcome back to MovieRecs!"
+        else 
+            redirect_to new_user_session_path
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_url
+    end
+
+end
