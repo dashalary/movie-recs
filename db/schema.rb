3fa9f9b7ac900e1110b8c263bfd76646c58785b5
Dasha Lary <dasha4988@gmail.com>
2020-11-27 19:56:20 -0500

    created categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_001815) do
+ActiveRecord::Schema.define(version: 2020_11_28_005558) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
