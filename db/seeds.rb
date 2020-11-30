191fbf7ef00b8bc88a04efd2f827ca2ccf9defa6
Dasha Lary <dasha4988@gmail.com>
2020-11-30 11:32:17 -0500

    created public categories and seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Romance")
+Category.create(name: "Foreign Film")
+Category.create(name: "Scary")
+Category.create(name: "Suspense")
+Category.create(name: "Funny")
+Category.create(name: "Documentary")
+
