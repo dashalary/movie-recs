e5548b00ba51f0610f7e67ec639a4dad01a7f0cf
Dasha Lary <dasha4988@gmail.com>
2020-12-03 11:57:06 -0500

    included error messages for movie class

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
     scope :seen, -> { where(seen: true) }
     accepts_nested_attributes_for :category, :reject_if => :all_blank, :allow_destroy => true
     validates :title, presence: true
+    validates :release_year, presence: true 
+    validates :category, presence: true
 end
