4e09569db054e70893b4a82719b983fdb0ec4198
Dasha Lary <dasha4988@gmail.com>
2021-05-03 12:56:41 -0400

    fixed category movies view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'movies/seen', to: 'movies#seen'
   # get 'movies/oldest', to: 'movies#oldest'
   resources :movies
-  resources :categories, only: [:index, :show, :new]  do 
+  resources :categories, only: [:index, :show]  do 
     resources :movies, only: [:index, :show, :new]
   end 
 
